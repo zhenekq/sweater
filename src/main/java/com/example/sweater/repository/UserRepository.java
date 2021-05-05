211f8216051509bdf4eeee6768bd54432e0667af
zhenekns <35662589+zhenekq@users.noreply.github.com>
2021-05-05 19:19:55 +0300

    start application with registration login and logout

---
+++ src/main/java/com/example/sweater/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package com.example.sweater.repository;
+
+import com.example.sweater.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByUsername(String username);
+}
